c3ed197299982bbc65e6e746d714bc31ee59b85f
rk <471091127@qq.com>
2017-12-28 17:28:06 +0800

    app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const http=require('superagent')
 const pug=require('pug')
+const fs=require('fs')
 const bodyParser=require('body-parser')
 const cheerio=require('cheerio')
 const app=express()
@@ -15,21 +16,53 @@ app.get('/',(req,res)=>{
         if(err){
             return next(err)
         }
+        let cnodeList=[]
         let $=cheerio.load(data.text)
         let cells=$('#topic_list .cell')
-        let list=[]
         cells.each((index,item)=>{
             let $item=$(item)
-            list.push({
+            cnodeList.push({
+                title:$item.find('.topic_title').text(),
+                id:$item.find('.topic_title').attr('href'),
                 userImg: $item.find('.user_avatar img').attr('src'),
                 replyCount:$item.find('.count_of_replies').text(),
-                visitCount:$item.find('.count_of_visits').text(),
-                title:$item.find('.topic_title').text()
+                visitCount:$item.find('.count_of_visits').text(),               
             })
         })
         res.render('index',{
-            title:'node',
-            list:list
+            title:'cNode',
+            cnodeList:cnodeList
+        })
+    })
+})
+app.get('/zhihu',(req,res)=>{
+    http.get('https://news-at.zhihu.com/api/4/news/latest')
+    .end((err,data)=>{
+        if(err){
+            return next(err)
+        }           
+        let zhihuList=[]
+        data=data.body.stories
+        data.forEach((item,index)=>{
+            http.get(item.images[0])
+            .set({'async':false})
+            .end((mis,rec)=>{
+                if(mis){
+                    return next(mis)
+                }
+                rec.setEncoding("binary")
+                fs.writeFile(__dirname+'/public/img/'+item.id+'.png',rec.body,'binary',()=>{
+                })
+            })
+            zhihuList.push({
+                id:item.id,
+                title:item.title,
+                img:item.images[0]
+            })
+        })
+        res.render('zhihu',{
+            title:'知乎日报',
+            zhihuList:zhihuList
         })
     })
 })
